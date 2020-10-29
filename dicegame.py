adbb642147b919810cdecb356de8f3099660885e
tushar-9999 <73636740+tushar-9999@users.noreply.github.com>
2020-10-29 17:04:40 +0530

    dice game

---
+++ dicegame.py
@@ -0,0 +1,19 @@
+#This is rolling dice game
+
+import random
+
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print ("Rolling the dices..." )
+    print ("The values are...")
+    print (random.randint (min,max))
+    print (random.randint (min,max))
+
+
+    roll_again = input("Roll the dices again? ")
+else:
+    print ("Thank you for playing this wonderful game! ")
